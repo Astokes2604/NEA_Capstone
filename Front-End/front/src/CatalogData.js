3d8450febb2a727cab23ebe7efbb8c221201ef5d
Astokes2604 <129238930+Astokes2604@users.noreply.github.com>
2024-06-26 14:41:07 -0600

    CatalogData
    temporary data to test before I set up the mongodb to hold all the clothing

---
+++ Front-End/front/src/CatalogData.js
@@ -0,0 +1,16 @@
+const catalogData = [
+    {
+        id: 1,
+        name: "Oversized Butterfly T-Shirt",
+        price: 30.00,
+        image: "./images/NewEraButterflies.png"
+    },
+    {
+        id: 2,
+        name: "New Era Positive Crop Top",
+        price: 25.00,
+        image: "./images/NewEraPositive.png"
+    }
+];
+
+export default catalogData;
